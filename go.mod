13621f810a4693aad6015770a78f9a2490129499
karl <enriquezkharl14@gmail.com>
2026-01-30 12:34:34 +0800

    [Feat] Post model

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/kharljhon14/socials
 
 go 1.25.0
 
-require github.com/go-chi/chi/v5 v5.2.4 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.11.1 // indirect
+)
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,50 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
+
+type Post struct {
+	ID        uuid.UUID `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserID    uuid.UUID `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
 
 type PostgresPostsStore struct {
 	db *sql.DB
 }
 
-func (s *PostgresPostsStore) Create(ctx context.Context) error {
+func (s *PostgresPostsStore) Create(ctx context.Context, post *Post) error {
+	query := `
+		INSERT INTO posts
+		(content, title, user_id, tags)
+		VALUES
+		($1, $2, $3, $4)
+		RETURNING id, created_at, updated_at
+	`
+
+	args := []any{
+		post.Content,
+		post.Title,
+		post.UserID,
+		pq.Array(post.Tags),
+	}
+
+	err := s.db.QueryRowContext(ctx, query, args...).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
