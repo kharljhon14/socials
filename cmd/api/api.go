d9efcc4dab5d6ddf7b054e3621fa0fac18505f5f
karl <enriquezkharl14@gmail.com>
2026-01-30 10:42:26 +0800

    [Feat] Routers

---
+++ cmd/api/api.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthcheckHandler)
+
+	})
+	return r
+
+}
+
+func (app *application) serve(handler http.Handler) error {
+
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      handler,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kharljhon14/socials
+
+go 1.25.0
+
+require github.com/go-chi/chi/v5 v5.2.4 // indirect
