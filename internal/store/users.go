00a86af3fd8487a535cd91f608debb838db7ee8a
karl <enriquezkharl14@gmail.com>
2026-01-30 12:53:51 +0800

    [Feat] User model

--- internal/store/users.go
+++ internal/store/users.go
@@ -3,12 +3,47 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
 )
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"_"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"upated_at"`
+}
 
 type PostgresUsersStore struct {
 	db *sql.DB
 }
 
-func (s *PostgresUsersStore) Create(ctx context.Context) error {
+func (s *PostgresUsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users
+		(username, email, password)
+		VALUES
+		($1, $2, $3)
+		RETURNING id, created_at, updated_at
+	`
+
+	args := []any{
+		user.Username,
+		user.Email,
+		user.Password,
+	}
+
+	err := s.db.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
